20834ef747bbae40ba28ca671bcf1ec0e37a058b
Danish Akram <danishakram79@gmail.com>
2022-01-28 14:43:16 +0500

    php artisan make:model -a

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::apiResource('/product',ProductController::class);
+
+Route::prefix('product')->group(function () {
+    Route::apiResource('/{product}/review', ReviewController::class);
 });
