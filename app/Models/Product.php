3bf49649e1942c099e096ab9f1ad6a113ffd2532
Danish Akram <danishakram79@gmail.com>
2022-02-07 18:56:08 +0500

    data transform

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Review;
 class Product extends Model
 {
-    use HasFactory;
-
     public function review()
     {
        return $this->hasMany(Review::class);
